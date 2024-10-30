ab134d116121455fb257ffd3446a06a5a4c7f9f6
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2024-10-30 20:17:51 -0300

    fix: fixed name

--- stacks/main.c
+++ stacks/main.c
@@ -1,72 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int queue_max_size = 3;
+const int stack_max_size = 3;
 
-const int queue_full = -5;
-const int queue_ok = 1;
+const int stack_status_full = -5;
+const int stack_status_ok = 1;
+
+const int stack_place_start = -1;
 
 typedef struct {
     int* head;
     int* items;
     int* tail;
     int icount;
-} Queue;
+} Stack;
 
-Queue* queue_create() {
-    Queue* q = malloc(sizeof(Queue));
+Stack* stack_create() {
+    stack* stack = malloc(sizeof(Stack));
 
-    q->items = malloc(sizeof(int) * queue_max_size);
-    q->head = &q->items[0];
+    stack->items = malloc(sizeof(int) * stack_max_size);
+    stack->head = stack_place_start;
     /* porque quando tem só uma pessoa na fila */
     /* ela é a primeira e última na fila */
-    q->tail = q->head;
+    stack->tail = stack_place_start;
     /* porque não tem ningúem na fila ainda */
-    q->icount = 0;
+    stac->icount = 0;
 
     return q;
 }
 
-int push(Queue* q, int value) {
-    if(q->icount == queue_max_size) {
-        return queue_full;
+int push(stack* q, int value) {
+    if(q->icount == stack_max_size) {
+        return stack_status_full;
     }
 
     q->items[q->icount] = value;
     q->tail = q->tail + 1;
     q->icount++;
 
-    return queue_ok;
+    return stack_status_ok;
 }
 
-void queue_print(Queue* q) {
+void stack_print(stack* q) {
     for(int i = 0; i < q->icount; i++) {
         printf("- %d\n", q->items[i]);
     }
 }
 
 int main() {
-    Queue* queue = queue_create();
+    stack* stack = stack_create();
 
-    if(push(queue, 1) == queue_full) {
-        printf("Queue is full.\n");
+    if(push(stack, 1) == stack_status_full) {
+        printf("stack is full.\n");
         return 1;
     }
 
-    if(push(queue, 2) == queue_full) {
-        printf("Queue is full.\n");
+    if(push(stack, 2) == stack_status_full) {
+        printf("stack is full.\n");
         return 1;
     }
 
-    if(push(queue, 3) == queue_full) {
-        printf("Queue is full.\n");
+    if(push(stack, 3) == stack_status_full) {
+        printf("stack is full.\n");
         return 1;
     }
 
-    queue_print(queue);
+    stack_print(stack);
 
-    free(queue);
-    queue = NULL;
+    free(stack);
+    stack = NULL;
 
     return 0;
 }
