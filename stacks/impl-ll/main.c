f995d02bf63a2f985e6f3a8a7fb452f136a13ff4
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-10-31 14:43:34 -0300

    feat: changed things, added err handling

--- stacks/impl-ll/main.c
+++ stacks/impl-ll/main.c
@@ -4,9 +4,10 @@
 typedef int StackStatus;
 typedef int StackData;
 
-const int stack_max_size = 3;
+const int stack_max_length = 3;
 
 StackStatus stack_status_empty = -5;
+StackStatus stack_max_reached = -4;
 StackStatus stack_status_ok = 1;
 
 typedef struct StackElem {
@@ -16,30 +17,33 @@ typedef struct StackElem {
 
 typedef struct {
     StackElem* head;
-    int count;
+    int length;
 } Stack;
 
 Stack* stack_create() {
     Stack* stack = malloc(sizeof(Stack));
-
     stack->head = NULL;
-    stack->count = 0;
-
+    stack->length = 0;
     return stack;
 }
 
-void push(Stack* stack, int value) {
-    StackElem* elem = malloc(sizeof(StackElem));
+StackStatus push(Stack* stack, int value) {
+    StackElem* elem = NULL;
 
+	if(stack->length == stack_max_length) {
+		return stack_max_reached;
+	}
+
+	elem = malloc(sizeof(StackElem));
     elem->data = value;
     elem->next = stack->head;
     stack->head = elem;
+    stack->length++;
 
-    stack->count++;
+	return stack_status_ok;
 }
 
 StackStatus pop(Stack* stack) {
-    StackElem* elems = malloc(sizeof(StackElem));
     StackElem* aux = NULL;
 
     if(stack->head == NULL) {
@@ -48,30 +52,29 @@ StackStatus pop(Stack* stack) {
 
     aux = stack->head;
 
-    elems->next = NULL;
-
-    /* go to the last element */
+    /* checks if the next stack element after the current is the end */
     while(aux->next->next != NULL) {
         aux = aux->next;
     }
 
     free(aux->next);
     aux->next = NULL;
-    stack->count--;
+    stack->length--;
 
     return stack_status_ok;
 }
 
 void stack_print(Stack* stack) {
+	StackElem* elemns = stack->head;
     int i;
 
-    printf("This stack has %d items.\n", stack->count);
+    printf("This stack has %d items.\n", stack->length);
 
     printf("-----------------------------------------\n");
 
-    for(i = 0; i < stack->count; i++) {
-        printf("- %d\n", stack->head->data);
-        stack->head = stack->head->next;
+    for(i = 0; i < stack->length; i++) {
+        printf("- %d\n", elemns->data);
+        elemns = elemns->next;
     }
 
     printf("-----------------------------------------\n");
@@ -80,9 +83,21 @@ void stack_print(Stack* stack) {
 int main() {
     Stack* stack = stack_create();
 
-    push(stack, 1);
-    push(stack, 2);
-    push(stack, 3);
+    if(push(stack, 1) == stack_max_reached) {
+		printf("max_size has been reached.\n");
+	}
+
+    if(push(stack, 2) == stack_max_reached) {
+		printf("max_size has been reached.\n");
+	}
+
+    if(push(stack, 3) == stack_max_reached) {
+		printf("max_size has been reached.\n");
+	}
+
+	if(push(stack, 4) == stack_max_reached) {
+		printf("max_size has been reached.\n");
+	}
 
     pop(stack);
 
