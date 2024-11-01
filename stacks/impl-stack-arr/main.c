904752ef064100cba2b027175af7e56a236815f0
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-11-01 16:37:03 -0300

    feat: added stack based stack dt implementation

---
+++ stacks/impl-stack-arr/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* now, let's try to create a stack with an arr */
+
+typedef int StackStatus;
+typedef const int StackConstant;
+
+StackConstant empty = 0;
+StackConstant def_size = 5; 
+
+StackStatus stack_limit_reached = -5;
+StackStatus stack_empty = -4;
+StackStatus stack_op_success = 1;
+
+typedef struct {
+	int top;
+	int items[5];
+} Stack;
+
+Stack stack_create() {
+	Stack stack;
+	stack.top = empty;
+	return stack;
+}
+
+StackStatus push(Stack stack, int value) {
+	if(stack.top == def_size) {
+		return stack_limit_reached;
+	}
+
+	stack.items[stack.top] = value;
+	stack.top++;
+
+	return stack_op_success;
+}
+
+StackStatus pop(Stack stack) {
+	if(stack.top == empty) {
+		return stack_empty;
+	}
+
+	stack.top--;
+	/* goes to the end of the stack */
+	stack.items[stack.top - 1] = empty;
+
+	return stack_op_success;
+}
+void stack_print(Stack stack) {
+	for(int i = 0; i < stack.top; i++) {
+		printf("%d\n", stack.items[i]);
+	}
+}
+
+int main() {
+	Stack stack = stack_create();
+
+	if(pop(stack) == stack_empty) {
+		printf("Stack is empty: no value to pop.\n");
+	}
+
+	if(push(stack, 1) == stack_limit_reached) {
+		printf("Stack is full\n");
+	}
+
+	if(push(stack, 2) == stack_limit_reached) {
+		printf("Stack is full\n");
+	}
+
+	stack_print(stack);
+
+	return 0;
+}
