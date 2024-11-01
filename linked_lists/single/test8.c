89a5b172c68881702c0654fd816cbf34a7ab2928
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-11-01 15:51:32 -0300

    feat: added a function to check if the head is null

--- linked_lists/single/test8.c
+++ linked_lists/single/test8.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 /* again code obtained from teacher Douglas */
 /* let's try to erase the tail, now */
-
 
 typedef int ListStat;
 
@@ -20,17 +20,18 @@ typedef struct {
 	Node* next;
 } List;
 
+bool is_link_empty(Node* link) {
+	return (link == NULL);
+}
+
 List* list_create() {
 	List* list = malloc(sizeof(List));
-
 	list->next = NULL;
-
 	return list;
 }
 
 void node_add_head(List* list, int value) {
 	Node* new_head = malloc(sizeof(Node));
-
 	new_head->value = value;
 	new_head->head = list->next;
 	list->next = new_head;
@@ -38,62 +39,49 @@ void node_add_head(List* list, int value) {
 
 ListStat node_add_tail(List* list, int value) {
 	Node* new_tail = malloc(sizeof(Node));
-	Node* aux;
-
-	if(list->next == NULL) {
+	Node* aux = NULL;
+	if(is_link_empty(list->next)) {
 		return list_empty;
 	}
-
 	aux = list->next;
-
 	new_tail->value = value;
 	new_tail->head = NULL;
-
 	/* mv end */
-	while(aux->head != NULL) {
+	while(!is_link_empty(aux->head)) {
 		aux = aux->head;
 	}
-
 	aux->head = new_tail;
-
 	return list_operation_succ;
 }
 
 ListStat node_rm_tail(List* list) {
-	Node* elements;
-
-	if(list->next == NULL) {
+	Node* elements = NULL;
+	if(is_link_empty(list->next)) {
 		return list_empty;
 	}
-
 	elements = list->next;
-
 	/* does list contains only one element */
 	/* if it has, then it is impossible to continue */
 	/* because then we can't get to the second to last element */
-	if(elements->head == NULL) {
+	if(is_link_empty(elements->head)) {
 		return list_imposs_rm;
 	}
-
 	/* will go one ahead, so if i have "1, 2, 3" */
 	/* and i'm at the start, then this will use the third element of the list */
 	/* and see if there is something else (which does not) */
 	/* "->head" goes to the next element, remember? */
-	while(elements->head->head != NULL) {
+	while(!is_link_empty(elements->head->head)) {
 		elements = elements->head;
 	} /* second to last element here */
-
 	/* free last element */
 	free(elements->head);
 	elements->head = NULL;
-
 	return list_operation_succ;
 }
 
 void list_print(List* list) {
 	Node* node = list->next;
-
-	while(node != NULL) {
+	while(!is_link_empty(node)) {
 		printf("%d\n", node->value);
 		node = node->head;
 	}
@@ -101,29 +89,6 @@ void list_print(List* list) {
 
 int main() {
 	List* list = list_create();
-	int list_op_stat;
-
-	node_add_head(list, 1);
-	node_add_head(list, 2);
-	node_add_head(list, 3);
-
-	list_op_stat = node_rm_tail(list);
-	list_op_stat = node_add_tail(list, 414);
-	list_op_stat = node_add_tail(list, 413);
-
-	if(list_op_stat == list_empty) {
-		printf("Could not add element to the end of the list. List is empty\n");
-	}
-
-	if(list_op_stat == list_empty) {
-		printf("Could not add element to the end of the list. List is empty\n");
-	}
-
-	if(list_op_stat == list_empty || list_op_stat == list_imposs_rm) {
-		printf("Could not remove element in the end of list. List length err.\n");
-	}
-
 	list_print(list);
-
 	return 0;
 }
