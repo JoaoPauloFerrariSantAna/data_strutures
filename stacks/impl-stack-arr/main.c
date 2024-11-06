fba421fa20fb1b2509ab56a693b192d83196f6aa
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-11-06 14:47:32 -0300

    fix: stack is now on heap

--- stacks/impl-stack-arr/main.c
+++ stacks/impl-stack-arr/main.c
@@ -18,46 +18,46 @@ typedef struct {
 	int items[5];
 } Stack;
 
-Stack stack_create() {
-	Stack stack;
-	stack.top = empty;
+Stack* stack_create() {
+	Stack* stack = malloc(sizeof(Stack));
+
+	stack->top = empty;
+
 	return stack;
 }
 
-StackStatus push(Stack stack, int value) {
-	if(stack.top == def_size) {
+StackStatus push(Stack* stack, int value) {
+	if(stack->top == def_size) {
 		return stack_limit_reached;
 	}
 
-	stack.items[stack.top] = value;
-	stack.top++;
+	stack->items[stack->top] = value;
+	stack->top++;
 
 	return stack_op_success;
 }
 
-StackStatus pop(Stack stack) {
-	if(stack.top == empty) {
+StackStatus pop(Stack* stack) {
+	if(stack->top == empty) {
 		return stack_empty;
 	}
 
-	stack.top--;
-	/* goes to the end of the stack */
-	stack.items[stack.top - 1] = empty;
 
+	/* goes to the end of the stack */
+	stack->items[stack->top - 1] = empty;
+
+	stack->top--;
 	return stack_op_success;
 }
-void stack_print(Stack stack) {
-	for(int i = 0; i < stack.top; i++) {
-		printf("%d\n", stack.items[i]);
+void stack_print(Stack* stack) {
+	for(int i = 0; i < stack->top; i++) {
+		printf("%d\n", stack->items[i]);
 	}
 }
 
 int main() {
-	Stack stack = stack_create();
+	Stack* stack = stack_create();
 
-	if(pop(stack) == stack_empty) {
-		printf("Stack is empty: no value to pop.\n");
-	}
 
 	if(push(stack, 1) == stack_limit_reached) {
 		printf("Stack is full\n");
@@ -65,6 +65,10 @@ int main() {
 
 	if(push(stack, 2) == stack_limit_reached) {
 		printf("Stack is full\n");
+	}
+
+	if(pop(stack) == stack_empty) {
+		printf("Stack is empty.\n");
 	}
 
 	stack_print(stack);
