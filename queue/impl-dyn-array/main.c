af786e05e0bf9b557054b380c515d831b0075999
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-11-08 11:05:42 -0300

    feat: updated dynamic array test

--- queue/impl-dyn-array/main.c
+++ queue/impl-dyn-array/main.c
@@ -1,30 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int magic_size = 5;
+typedef int QueueLength;
+typedef int QueueNewLength;
+typedef int* Items;
+
+QueueNewLength queue_new_limit = 2;
+const int queue_empty = 0;
 
 typedef struct {
-	/* because i want to know of who enters first */
-	int* head;
-	int* elements;
-	/* because i want to know of who enters last */
-	int* tail;
+	int back;
+	/* because then we will need to update where the front is */
+	int front;
+	QueueLength length;
+	Items items;
 } Queue;
 
-Queue* queue_create(int length) {
+void queue_copy_to(Items dest, Items source, int until) {
+	int i;
+	for(i = 0; i < until; i++) {
+		dest[i] = source[i];
+	}
+}
+
+Items queue_resize(Items old_items, QueueLength old_space, int space) {
+	QueueNewLength new_size = old_space + space;
+	Items new_items = malloc(sizeof(int) * new_size);
+	queue_copy_to(new_items, old_items, old_space + 1);
+	free(old_items);
+	return new_items;
+}
+
+Queue* queue_create(QueueLength length) {
 	Queue* queue = malloc(sizeof(Queue));
 
-	queue->elements = malloc(sizeof(int) * length);
-	queue->head = &queue->elements[0];
-
-	/* we know that in normal arrays the last element is the length - 1 */
-	queue->tail = &queue->elements[length - 1];
+	queue->front = 0;
+	queue->back = 0;
+	queue->length = length;
+	queue->items = malloc(sizeof(int) * length);
 
 	return queue;
 }
 
+void enqueue(Queue* queue, int value) {
+	if(queue->front == queue->length) {
+		queue->items = queue_resize(queue->items, queue->length, queue_new_limit);
+		queue->length += queue_new_limit;
+	}
+	queue->items[queue->front] = value;
+	/* we need to update the front to keep track of the current item */
+	/* because the front is always the most recent one */
+	queue->front++;
+}
+
+void dequeue(Queue* queue) {
+	if(queue->length == queue_empty) {
+		printf("Underflow of the queue.\n");
+		return;
+	}
+
+	queue->length--;
+	queue->front--;
+	queue->items[queue->front] = 0;
+}
+
+void queue_print(Queue* queue) {
+	int i;
+	for(i = 0; i < queue->length; i++) {
+		printf("%d\n", queue->items[i]);
+	}
+}
+
 int main() {
-	Queue* queue = queue_create(magic_size);
+	Queue* queue = queue_create(2);
 
 	free(queue);
 	queue = NULL;
