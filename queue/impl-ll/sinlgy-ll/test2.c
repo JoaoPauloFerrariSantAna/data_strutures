09cb0c5303ad0710151d4a0a747553a8b850901e
GhostlyTrincket <joaopauloferrarisantana@gmail.com>
2024-11-08 10:27:40 -0300

    feat: added a new type and function

--- queue/impl-ll/sinlgy-ll/test2.c
+++ queue/impl-ll/sinlgy-ll/test2.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef int QueueData;
 
 /* first we'll start with a "queue element" */
 typedef struct QueueElement {
 	struct QueueElement* tail;
-	int value;
+	QueueData value;
 } QueueElement;
 
 /* now let's create the actual queue */
@@ -12,6 +15,10 @@ typedef struct {
 	QueueElement* prev;
 	int length;
 } Queue;
+
+bool is_link_empty(QueueElement* link) {
+	return (link == NULL);
+}
 
 /* let's create a queue */
 Queue* queue_create() {
@@ -28,7 +35,7 @@ void queue_print(Queue* q) {
 
 	printf("Queue has length: %d\n", q->length);
 
-	while(elements != NULL) {
+	while(is_link_empty(elements)) {
 		printf("%d\n", elements->value);
 
 		/* this is saying "go back" */
@@ -39,7 +46,7 @@ void queue_print(Queue* q) {
 
 /* we insert a value in a queue at the end, soooo... */
 /* we must make a insertion like in a tail of a singly linked list */
-void enqueue(Queue* q, int value) {
+void enqueue(Queue* q, QueueData value) {
 	/* we must create a "QueueElement" */
 	QueueElement* qelement = malloc(sizeof(QueueElement));
 	QueueElement* aux = NULL;
@@ -49,7 +56,7 @@ void enqueue(Queue* q, int value) {
 
 	/* if there's nothing at the end */ 
 	/* then we must set "->prev" to track qelement as the first element */
-	if(q->prev == NULL) {
+	if(is_link_empty(q->prev)) {
 		q->prev = qelement;
 		return;
 	}
@@ -57,14 +64,15 @@ void enqueue(Queue* q, int value) {
 	aux = q->prev;
 
 	/* first, we'll go to the end of the queue */
-	while(aux->tail != NULL) {
-		aux = aux->tail;
-	} /* from here on out we are at the last element */
+	while(!is_link_empty(aux->tail)) { aux = aux->tail; }
 
+	/* from here on out we are at the last element */
 	aux->tail = qelement;
 
 	q->length++;
 }
+
+
 
 /* now let's remove a element of the queue */
 void dequeue(Queue* q) {
@@ -72,9 +80,14 @@ void dequeue(Queue* q) {
 	/* after the current is the tail */
 	QueueElement* aux = q->prev;
 
+	if(aux == NULL) {
+		/* queue is empty */
+		return;
+	}
+
 	/* so.. while the element after the current one */
 	/* is not empty, continue to advance on the queue */
-	while(aux->tail->tail != NULL) { aux = aux->tail; }
+	while(!is_link_empty(aux->tail->tail)) { aux = aux->tail; }
 
 	/* removing the front */
 	free(aux->tail);
